a112d7221a59ea7401a9282dcbbed49f067f8f75
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-01-20 23:35:21 +0530

    `RemoteSalonLoader` requests data from URL on `load`

--- NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
@@ -7,8 +7,18 @@
 
 import XCTest
 
-class RemoteSalonLoader {
+private final class RemoteSalonLoader {
+    private let url: URL
+    private let client: HTTPClient
     
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.requestedURL = url
+    }
 }
 
 class HTTPClient {
@@ -18,8 +28,17 @@ class HTTPClient {
 final class RemoteSalonLoaderTests: XCTestCase {
     func test_init_doesnotRequestDataFromURL() {
         let client = HTTPClient()
-        let _ = RemoteSalonLoader()
+        let _ = RemoteSalonLoader(url: URL(string: "http://a-url.com")!, client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteSalonLoader(url: URL(string: "http://a-url.com")!, client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
