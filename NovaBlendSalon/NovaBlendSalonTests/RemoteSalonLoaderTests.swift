af6766403418315a6cc13d8fa65bec9972966035
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-01-21 10:32:48 +0530

    `RemoteSalonLoader` delivers connectivity error on client error

--- NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
@@ -11,18 +11,24 @@ private final class RemoteSalonLoader {
     private let url: URL
     private let client: HTTPClient
     
+    enum Error {
+        case connectivity
+    }
+    
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    func load() {
-        client.getFrom(url: url)
+    func load(completion: @escaping (Error) -> Void) {
+        client.getFrom(url: url) { error in
+            completion(.connectivity)
+        }
     }
 }
 
 protocol HTTPClient {
-    func getFrom(url: URL)
+    func getFrom(url: URL, completion: @escaping (Error) -> Void)
 }
 
 final class RemoteSalonLoaderTests: XCTestCase {
@@ -33,8 +39,7 @@ final class RemoteSalonLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL() {
         let (sut,client) = makeSUT()
-        sut.load()
-        
+        sut.load { _ in }
         XCTAssertFalse(client.requestedURLs.isEmpty)
     }
     
@@ -42,10 +47,22 @@ final class RemoteSalonLoaderTests: XCTestCase {
         let url = URL(string: "http://a-url.com")!
         let (sut,client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load { _ in }
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversConnectivityErrorOnClientError() {
+        let (sut,client) = makeSUT()
+        
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteSalonLoader.Error?
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
     }
 }
 
@@ -58,8 +75,12 @@ private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticSt
 
 private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
+    var error: Error?
     
-    func getFrom(url: URL) {
+    func getFrom(url: URL, completion: @escaping (Error) -> Void) {
+        if let error = error {
+            completion(error)
+        }
         requestedURLs.append(url)
     }
 }
