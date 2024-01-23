765da3ea7b6bf38bd61c0a00922d61e23e325ede
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-01-23 10:13:58 +0530

    Update `HTTPClientSpy` to use `Result` type

--- NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/RemoteSalonLoaderTests.swift
@@ -11,19 +11,19 @@ import NovaBlendSalon
 final class RemoteSalonLoaderTests: XCTestCase {
     
     func test_init_doesnotRequestDataFromURL() {
-        let (_,client) = makeSUT()
+        let (_,client) = makeSUT(with: .success(()))
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestDataFromURL() async throws{
-        let (sut,client) = makeSUT()
+        let (sut,client) = makeSUT(with: .success(()))
         try await sut.load()
         XCTAssertFalse(client.requestedURLs.isEmpty)
     }
     
     func test_loadTwice_requestDataFromURLTwice() async throws{
         let url = URL(string: "http://a-url.com")!
-        let (sut,client) = makeSUT(url: url)
+        let (sut,client) = makeSUT(url: url, with: .success(()))
         
         try await sut.load()
         try await sut.load()
@@ -32,8 +32,8 @@ final class RemoteSalonLoaderTests: XCTestCase {
     }
     
     func test_load_deliversConnectivityErrorOnClientError() async throws {
-        let (sut,client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
+        let error = NSError(domain: "Test", code: 0)
+        let (sut,_) = makeSUT(with: .failure(error))
         
         do {
             try await sut.load()
@@ -44,20 +44,23 @@ final class RemoteSalonLoaderTests: XCTestCase {
 }
 
 //MARK: Helper
-private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteSalonLoader, client: HTTPClientSpy) {
-    let client = HTTPClientSpy()
+private func makeSUT(url: URL = URL(string: "http://a-url.com")!,with result: Result<Void, Error>, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteSalonLoader, client: HTTPClientSpy) {
+    let client = HTTPClientSpy(result: result)
     let sut = RemoteSalonLoader(url: url, client: client)
     return(sut, client)
 }
 
 private class HTTPClientSpy: HTTPClient {
     private(set) var requestedURLs = [URL]()
-    var error: Error?
+    let result: Result<Void, Error>
+    
+    init(requestedURLs: [URL] = [URL](), result: Result<Void, Error>) {
+        self.requestedURLs = requestedURLs
+        self.result = result
+    }
     
     func getFrom(url: URL) async throws {
-        if let error = error {
-            throw error
-        }
         requestedURLs.append(url)
+        return try result.get()
     }
 }
