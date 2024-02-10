2b344985abed2358d9a06ad9da1a089e93f40c96
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-02-10 20:56:30 +0530

    delivers retrival error on retrival failure

--- NovaBlendSalon/NovaBlendSalonTests/Salon Cache/LoadSalonFromCacheUseCaseTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/Salon Cache/LoadSalonFromCacheUseCaseTests.swift
@@ -8,22 +8,40 @@
 import XCTest
 
 private class SalonStore {
-    var receivedMessages = 0
+    typealias Result = Error?
     
-    func retrieve() {
+    var receivedMessages = 0
+    let result: Result
+    
+    init(result: Result) {
+        self.result = result
+    }
+    
+    func retrieve() async throws {
         receivedMessages += 1
+        if result != nil {
+            throw result!
+        }
     }
 }
 
 private class LocalSalonLoader {
     let store: SalonStore
     
+    enum Error: Swift.Error {
+        case retrival
+    }
+    
     init(store: SalonStore) {
         self.store = store
     }
     
-    func load() {
-        store.retrieve()
+    func load() async throws {
+        do {
+            try await store.retrieve()
+        } catch  {
+            throw Error.retrival
+        }
     }
     
 }
@@ -35,26 +53,38 @@ final class LoadSalonFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, 0)
     }
     
-    func test_load_requestsCacheRetrival() {
+    func test_load_requestsCacheRetrival() async throws {
         let (sut, store) = makeSUT()
         
-        sut.load()
+        try await sut.load()
         
         XCTAssertEqual(store.receivedMessages, 1)
     }
     
-    func test_load_twice_requestsCacheRetrivalTwice() {
+    func test_load_twice_requestsCacheRetrivalTwice() async throws {
         let (sut, store) = makeSUT()
         
-        sut.load()
-        sut.load()
+        try await sut.load()
+        try await sut.load()
         
         XCTAssertEqual(store.receivedMessages, 2)
     }
     
+    func test_load_failsOnRetrivalError() async {
+        let (sut, _) = makeSUT(with: anyNSError())
+        
+        do {
+            _ = try await sut.load()
+            XCTFail("Expected to throw error but got success intead")
+        } catch {
+            XCTAssertEqual(error as? LocalSalonLoader.Error, .retrival)
+        }
+        
+    }
+    
     //MARK: Helpers
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalSalonLoader, store: SalonStore) {
-        let store = SalonStore()
+    private func makeSUT(with result: SalonStore.Result = nil,file: StaticString = #file, line: UInt = #line) -> (sut: LocalSalonLoader, store: SalonStore) {
+        let store = SalonStore(result: result)
         let sut = LocalSalonLoader(store: store)
         trackForMemoryLeak(store)
         trackForMemoryLeak(sut)
