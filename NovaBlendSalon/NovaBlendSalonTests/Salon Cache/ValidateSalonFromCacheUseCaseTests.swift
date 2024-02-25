57e10e66ad7e6e04b4223a9c2a83bdee3d6c0a4a
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-02-25 11:33:18 +0530

    LocalSalonLoader does not message store upon creation

---
+++ NovaBlendSalon/NovaBlendSalonTests/Salon Cache/ValidateSalonFromCacheUseCaseTests.swift
@@ -0,0 +1,26 @@
+//
+//  ValidateSalonFromCacheUseCaseTests.swift
+//  NovaBlendSalonTests
+//
+//  Created by Mushthak Ebrahim on 24/02/24.
+//
+
+import XCTest
+import NovaBlendSalon
+
+final class ValidateSalonFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(with result: SalonStoreSpy.Result = .success(([], Date())), currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalSalonLoader, store: SalonStoreSpy) {
+        let store = SalonStoreSpy(result: result)
+        let sut = LocalSalonLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeak(store)
+        trackForMemoryLeak(sut)
+        return(sut, store)
+    }
+}
