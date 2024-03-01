fa6552848fac55fc1141bb45252e376343c7e84c
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-03-01 18:32:14 +0530

    `validateCache` deletes expired cache

--- NovaBlendSalon/NovaBlendSalonTests/Salon Cache/ValidateSalonFromCacheUseCaseTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/Salon Cache/ValidateSalonFromCacheUseCaseTests.swift
@@ -53,6 +53,17 @@ final class ValidateSalonFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedSalons])
     }
     
+    func test_validateCache_deletesExpiredCache() async {
+        let salons = uniqueSalons()
+        let fixedCurrentDate = Date()
+        let expiredTimestamp = fixedCurrentDate.minusSalonCacheMaxAge().adding(seconds: -1)
+        let (sut, store) = makeSUT(with: .success((salons.local, expiredTimestamp)),currentDate: { fixedCurrentDate })
+        
+        await sut.validateCache()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedSalons])
+    }
+    
     //MARK: Helpers
     private func makeSUT(with result: SalonStoreSpy.Result = .success(([], Date())), currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalSalonLoader, store: SalonStoreSpy) {
         let store = SalonStoreSpy(result: result)
