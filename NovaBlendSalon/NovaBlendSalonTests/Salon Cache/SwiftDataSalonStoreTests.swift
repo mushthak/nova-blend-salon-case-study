dd56f95924a3f456b97eb2247f6590a6954a5084
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-06-24 22:39:57 +0530

    `SwiftDataSalonStore.retrieve()` delivers empty on empty cache

---
+++ NovaBlendSalon/NovaBlendSalonTests/Salon Cache/SwiftDataSalonStoreTests.swift
@@ -0,0 +1,48 @@
+//
+//  SwiftDataSalonStoreTests.swift
+//  NovaBlendSalonTests
+//
+//  Created by Mushthak Ebrahim on 24/06/24.
+//
+
+import Foundation
+import NovaBlendSalon
+import XCTest
+
+class SwiftDataSalonStore: SalonStore {
+    func retrieve() async throws -> NovaBlendSalon.CachedSalon {
+        return (salons: [], timestamp: Date.init())
+    }
+    
+    func deleteCachedSalons() async throws {
+        
+    }
+    
+    func insert(_ salons: [NovaBlendSalon.LocalSalonItem], timestamp: Date) async throws {
+        
+    }
+
+}
+
+class SwiftDataSalonStoreTests: XCTestCase {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() async {
+        let sut = makeSUT()
+        do {
+            let result = try await sut.retrieve()
+            XCTAssertEqual(result.salons, [])
+        } catch {
+            XCTFail("Expected success but got \(error) intead")
+        }
+    }
+
+    //MARK: Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> SalonStore {
+        let sut = SwiftDataSalonStore()
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+}
+
+
