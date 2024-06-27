75cce13b6e862f2d038baeab253e752198748ff9
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-06-27 22:17:14 +0530

    Add `NovaBlendSalonCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ NovaBlendSalon/NovaBlendSalonCacheIntegrationTests/NovaBlendSalonCacheIntegrationTests.swift
@@ -0,0 +1,10 @@
+//
+//  NovaBlendSalonCacheIntegrationTests.swift
+//  NovaBlendSalonCacheIntegrationTests
+//
+//  Created by Mushthak Ebrahim on 27/06/24.
+//
+
+import XCTest
+
+final class NovaBlendSalonCacheIntegrationTests: XCTestCase {}
