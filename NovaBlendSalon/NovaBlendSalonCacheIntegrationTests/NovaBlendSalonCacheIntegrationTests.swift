cd8218a48bba223190c44cbde5dde9b15cad8364
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-06-28 14:33:08 +0530

    `LocalSalonLoader` in integration with the `SwiftDataSalonStore` delivers no items on empty cache

--- NovaBlendSalon/NovaBlendSalonCacheIntegrationTests/NovaBlendSalonCacheIntegrationTests.swift
+++ NovaBlendSalon/NovaBlendSalonCacheIntegrationTests/NovaBlendSalonCacheIntegrationTests.swift
@@ -6,5 +6,28 @@
 //
 
 import XCTest
+import NovaBlendSalon
+import SwiftData
 
-final class NovaBlendSalonCacheIntegrationTests: XCTestCase {}
+final class NovaBlendSalonCacheIntegrationTests: XCTestCase {
+    func test_load_deliversNoItemsOnEmptyCache() async{
+        do {
+            let sut = makeSUT()
+            let result: [Salon] = try await sut.load()
+            XCTAssertEqual(result, [])
+        } catch {
+            XCTFail("Expected success but got \(error) intead")
+        }
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalSalonLoader {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: ManagedSalonItem.self, configurations: config)
+        let store = SwiftDataSalonStore(modelContainer: container)
+        let sut = LocalSalonLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeak(store)
+        trackForMemoryLeak(sut)
+        return sut
+    }
+}
