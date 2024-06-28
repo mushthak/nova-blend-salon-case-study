f14ff6174c9970803f3478110dedef8ef6f8f9c9
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-06-28 15:01:54 +0530

    `LocalSalonLoader` in integration with the `SwiftDataSalonStore` delivers items saved on separate instances, proving we correctly persist the data models

--- NovaBlendSalon/NovaBlendSalonCacheIntegrationTests/NovaBlendSalonCacheIntegrationTests.swift
+++ NovaBlendSalon/NovaBlendSalonCacheIntegrationTests/NovaBlendSalonCacheIntegrationTests.swift
@@ -12,7 +12,7 @@ import SwiftData
 final class NovaBlendSalonCacheIntegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() async{
         do {
-            let sut = makeSUT()
+            let sut = makeSUT(container: getContainer())
             let result: [Salon] = try await sut.load()
             XCTAssertEqual(result, [])
         } catch {
@@ -20,14 +20,33 @@ final class NovaBlendSalonCacheIntegrationTests: XCTestCase {
         }
     }
     
+    func test_load_deliversItemsSavedOnASeperateInstance() async{
+        do {
+            let container = getContainer()
+            let sutToPerformSave = makeSUT(container: container)
+            let sutToPerformLoad = makeSUT(container: container)
+            let salons = uniqueSalons().models
+            
+            try await sutToPerformSave.save(salons)
+            
+            let result: [Salon] = try await sutToPerformLoad.load()
+            XCTAssertEqual(result.count, salons.count)
+        } catch {
+            XCTFail("Expected success but got \(error) intead")
+        }
+    }
+    
     //MARK: Helpers
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalSalonLoader {
-        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try! ModelContainer(for: ManagedSalonItem.self, configurations: config)
+    private func makeSUT(container: ModelContainer, file: StaticString = #file, line: UInt = #line) -> LocalSalonLoader {
         let store = SwiftDataSalonStore(modelContainer: container)
         let sut = LocalSalonLoader(store: store, currentDate: Date.init)
         trackForMemoryLeak(store)
         trackForMemoryLeak(sut)
         return sut
+    }
+    
+    private func getContainer() -> ModelContainer {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        return try! ModelContainer(for: ManagedSalonItem.self, configurations: config)
     }
 }
