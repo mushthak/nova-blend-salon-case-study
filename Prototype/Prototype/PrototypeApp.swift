07f32d57b35a827cea121573052570f8b14330ca
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-01 17:59:25 +0530

    Add empty project for prototype

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Mushthak Ebrahim on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
