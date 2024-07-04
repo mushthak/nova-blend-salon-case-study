b1ac257f6d8ebf99862dc7467ca8c11318eb794d
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-04 21:51:53 +0530

    Add swiftUI `SalonListView` for salon listing screen

---
+++ NovaBlendSalon/NovaBlendSalonUI/Salon UI/SalonListView.swift
@@ -0,0 +1,18 @@
+//
+//  SalonListView.swift
+//  NovaBlendSalonUI
+//
+//  Created by Mushthak Ebrahim on 04/07/24.
+//
+
+import SwiftUI
+
+struct SalonListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    SalonListView()
+}
