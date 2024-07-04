eb61db547069f42ba20b446babf34f1115eea075
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-04 21:42:16 +0530

    Add `NovaBlendSalonUI` framework (prod and test) targets for ui-specific components

---
+++ NovaBlendSalon/NovaBlendSalonUI/NovaBlendSalonUI.h
@@ -0,0 +1,18 @@
+//
+//  NovaBlendSalonUI.h
+//  NovaBlendSalonUI
+//
+//  Created by Mushthak Ebrahim on 04/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NovaBlendSalonUI.
+FOUNDATION_EXPORT double NovaBlendSalonUIVersionNumber;
+
+//! Project version string for NovaBlendSalonUI.
+FOUNDATION_EXPORT const unsigned char NovaBlendSalonUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NovaBlendSalonUI/PublicHeader.h>
+
+
