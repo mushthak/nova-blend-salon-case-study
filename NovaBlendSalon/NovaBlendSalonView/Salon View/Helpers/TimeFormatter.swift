b00aa91f2ea310491eb9ef2333d2dfe00ef89ded
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-14 07:56:33 +0530

    Update `TimeFormatter` to handle precision values accuratly

--- NovaBlendSalon/NovaBlendSalonView/Salon View/Helpers/TimeFormatter.swift
+++ NovaBlendSalon/NovaBlendSalonView/Salon View/Helpers/TimeFormatter.swift
@@ -10,8 +10,12 @@ import Foundation
 public enum TimeFormatter {
     public static func convertTo12HourFormat(from time24: Float) -> String? {
         // Extract hours and minutes from the Float value
-        let hours = Int(time24)
-        let minutes = Int((time24 - Float(hours)) * 100) // Capture the decimal part as minutes
+        guard let roundedTime24 = Double(time24.formatted(.number.precision(.fractionLength(2)))) else { return "" }
+        
+        let hours = Int(roundedTime24)
+        
+        let diff = (Float(roundedTime24) - Float(hours)) * 100
+        let minutes = Int(Double(diff.formatted(.number.precision(.fractionLength(2))))!) // Capture the decimal part as minutes
         
         // Format the hours and minutes into a 24-hour time string
         let time24String = String(format: "%02d:%02d", hours, minutes)
