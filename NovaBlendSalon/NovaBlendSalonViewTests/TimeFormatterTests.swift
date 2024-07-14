8cd8a0d05e528a9fc483aa70402a1c8914d53d45
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-14 07:56:33 +0530

    Update `TimeFormatter` to handle precision values accuratly

--- NovaBlendSalon/NovaBlendSalonViewTests/TimeFormatterTests.swift
+++ NovaBlendSalon/NovaBlendSalonViewTests/TimeFormatterTests.swift
@@ -15,6 +15,7 @@ final class TimeFormatterTests: XCTestCase {
         XCTAssertEqual(TimeFormatter.convertTo12HourFormat(from: 0.00), "12:00 AM")
         XCTAssertEqual(TimeFormatter.convertTo12HourFormat(from: 12.00), "12:00 PM")
         XCTAssertEqual(TimeFormatter.convertTo12HourFormat(from: 23.59), "11:59 PM")
+        XCTAssertEqual(TimeFormatter.convertTo12HourFormat(from: 20.30), "8:30 PM")
         XCTAssertNil(TimeFormatter.convertTo12HourFormat(from: 24.00)) // Invalid time
     }
     
