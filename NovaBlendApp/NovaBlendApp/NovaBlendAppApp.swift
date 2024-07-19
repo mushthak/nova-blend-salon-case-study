dbca9ed281df57c7bb3ddaac45848f5e648323c2
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-07-19 18:26:19 +0530

    Add empty iOS app project to compose the application

---
+++ NovaBlendApp/NovaBlendApp/NovaBlendAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaBlendAppApp.swift
+//  NovaBlendApp
+//
+//  Created by Mushthak Ebrahim on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NovaBlendAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
