1e0e3d99950e7a3b96b9091fcfc55121086a7b78
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-01 16:42:00 +0530

    Add tolerance for snapshot comparison

--- NovaBlendSalon/NovaBlendSalonViewTests/Helpers/XCTestCase+Snapshot.swift
+++ NovaBlendSalon/NovaBlendSalonViewTests/Helpers/XCTestCase+Snapshot.swift
@@ -18,7 +18,7 @@ extension XCTestCase {
             return
         }
         
-        if snapshotData != storedSnapshotData {
+        if !match(snapshotData!, storedSnapshotData, tolerance: 0.9) {
             let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
                 .appendingPathComponent(snapshotURL.lastPathComponent)
             
@@ -59,6 +59,67 @@ extension XCTestCase {
         }
         
         return data
+    }
+    
+    private func match(_ oldData: Data, _ newData: Data, tolerance: Float = 0) -> Bool {
+        if oldData == newData { return true }
+        
+        guard let oldImage = UIImage(data: oldData)?.cgImage, let newImage = UIImage(data: newData)?.cgImage else {
+            return false
+        }
+        
+        guard oldImage.width == newImage.width, oldImage.height == newImage.height else {
+            return false
+        }
+        
+        let minBytesPerRow = min(oldImage.bytesPerRow, newImage.bytesPerRow)
+        let bytesCount = minBytesPerRow * oldImage.height
+        
+        var oldImageByteBuffer = [UInt8](repeating: 0, count: bytesCount)
+        guard let oldImageData = data(for: oldImage, bytesPerRow: minBytesPerRow, buffer: &oldImageByteBuffer) else {
+            return false
+        }
+        
+        var newImageByteBuffer = [UInt8](repeating: 0, count: bytesCount)
+        guard let newImageData = data(for: newImage, bytesPerRow: minBytesPerRow, buffer: &newImageByteBuffer) else {
+            return false
+        }
+        
+        if memcmp(oldImageData, newImageData, bytesCount) == 0 { return true }
+        
+        return match(oldImageByteBuffer, newImageByteBuffer, tolerance: tolerance, bytesCount: bytesCount)
+    }
+    
+    private func data(for image: CGImage, bytesPerRow: Int, buffer: UnsafeMutableRawPointer) -> UnsafeMutableRawPointer? {
+        guard
+            let space = image.colorSpace,
+            let context = CGContext(
+                data: buffer,
+                width: image.width,
+                height: image.height,
+                bitsPerComponent: image.bitsPerComponent,
+                bytesPerRow: bytesPerRow,
+                space: space,
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+            )
+        else { return nil }
+        
+        context.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        return context.data
+    }
+    
+    private func match(_ bytes1: [UInt8], _ bytes2: [UInt8], tolerance: Float, bytesCount: Int) -> Bool {
+        var differentBytesCount = 0
+        for i in 0 ..< bytesCount where bytes1[i] != bytes2[i] {
+            differentBytesCount += 1
+            
+            let percentage = Float(differentBytesCount) / Float(bytesCount)
+            if percentage > tolerance {
+                return false
+            }
+        }
+        return true
     }
     
 }
