ef9e10c14e726dcd1444a4d1d75098c8a2cd35ff
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-04 23:53:50 +0530

    Displays remote salons on launch when customer has connectivity

--- NovaBlendApp/NovaBlendApp/NovaBlendApp.swift
+++ NovaBlendApp/NovaBlendApp/NovaBlendApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import NovaBlendSalon
+import NovaBlendSalonView
 
 @main
 struct NovaBlendApp: App {
     var body: some Scene {
+        let remoteURL = URL(string: "https://mushthak.github.io/nova-salon-api/salons")!
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteSalonLoader = RemoteSalonLoader(url: remoteURL, client: remoteClient)
+        let salonListViewModelAdapter = SalonListViewModelAdapter(loader: remoteSalonLoader)
+        let salonListViewModel = SalonListViewModel(salonListViewModelAdapter: salonListViewModelAdapter)
+        
         WindowGroup {
-            ContentView()
+            SalonListView(viewModel: salonListViewModel)
         }
     }
 }
