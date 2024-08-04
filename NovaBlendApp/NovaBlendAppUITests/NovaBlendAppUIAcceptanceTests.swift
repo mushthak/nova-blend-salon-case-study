fab6bce14ccb7acbf380e9d4665be9b2932186fa
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-04 23:20:10 +0530

    Add UI Test target for running high-level Acceptance Tests

---
+++ NovaBlendApp/NovaBlendAppUITests/NovaBlendAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  NovaBlendAppUIAcceptanceTests.swift
+//  NovaBlendAppUITests
+//
+//  Created by Mushthak Ebrahim on 04/08/24.
+//
+
+import XCTest
+
+final class NovaBlendAppUIAcceptanceTests: XCTestCase {
+    
+}
