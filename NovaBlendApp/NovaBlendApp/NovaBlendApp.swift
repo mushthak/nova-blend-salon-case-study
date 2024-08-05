adbc17bd79b3f6f08c150ab5f33a702351c35da9
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-05 22:25:17 +0530

    Displays cached salons on launch when customer has no connectivity

--- NovaBlendApp/NovaBlendApp/NovaBlendApp.swift
+++ NovaBlendApp/NovaBlendApp/NovaBlendApp.swift
@@ -8,18 +8,45 @@
 import SwiftUI
 import NovaBlendSalon
 import NovaBlendSalonView
+import SwiftData
 
 @main
 struct NovaBlendApp: App {
     var body: some Scene {
         let remoteURL = URL(string: "https://mushthak.github.io/nova-salon-api/salons")!
-        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteClient = makeRemoteClient()
         let remoteSalonLoader = RemoteSalonLoader(url: remoteURL, client: remoteClient)
-        let salonListViewModelAdapter = SalonListViewModelAdapter(loader: remoteSalonLoader)
+        
+        let store = SwiftDataSalonStore(modelContainer: getContainer())
+        let localSalonLoader = LocalSalonLoader(store: store, currentDate: Date.init)
+        
+        let salonLoader = SalonLoaderWithFallbackComposite(primary: SalonLoaderCacheDecorator(decoratee: remoteSalonLoader, cache: localSalonLoader), fallback: localSalonLoader)
+        let salonListViewModelAdapter = SalonListViewModelAdapter(loader: salonLoader)
         let salonListViewModel = SalonListViewModel(salonListViewModelAdapter: salonListViewModelAdapter)
         
         WindowGroup {
             SalonListView(viewModel: salonListViewModel)
         }
+    }
+    
+    //MARK: Helpers
+    private func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            return AlwaysFailingHTTPClient()
+            
+        default:
+            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        }
+    }
+    
+    private class AlwaysFailingHTTPClient: HTTPClient {
+        func getFrom(url: URL) async throws -> (Data, HTTPURLResponse) {
+            throw NSError(domain: "offline", code: 0)
+        }
+    }
+    
+    private func getContainer() -> ModelContainer {
+        return try! ModelContainer(for: ManagedSalonItem.self)
     }
 }
--- NovaBlendApp/NovaBlendApp/SalonLoaderCacheDecorator.swift
+++ NovaBlendApp/NovaBlendApp/SalonLoaderCacheDecorator.swift
@@ -8,7 +8,7 @@
 import Foundation
 import NovaBlendSalon
 
-class SalonLoaderCacheDecorator {
+class SalonLoaderCacheDecorator : SalonLoader {
     private let decoratee: SalonLoader
     private let cache: SalonCache
     
