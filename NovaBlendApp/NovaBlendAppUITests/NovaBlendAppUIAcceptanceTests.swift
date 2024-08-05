0ef2c2f377dbc926786f70be0f496d6f1c80c160
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-05 22:25:17 +0530

    Displays cached salons on launch when customer has no connectivity

--- NovaBlendApp/NovaBlendAppUITests/NovaBlendAppUIAcceptanceTests.swift
+++ NovaBlendApp/NovaBlendAppUITests/NovaBlendAppUIAcceptanceTests.swift
@@ -15,4 +15,15 @@ final class NovaBlendAppUIAcceptanceTests: XCTestCase {
         
         XCTAssertEqual(app.cells.count, 4)
     }
+    
+    func test_onLaunch_displaysCachedRemoteSalonsWhenCustomerHasNoConnectivity() {
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+        
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+        
+        XCTAssertEqual(offlineApp.cells.count, 4)
+    }
 }
