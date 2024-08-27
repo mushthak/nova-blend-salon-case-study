aa71b972b45ed6ac7914f8c8f7c16263ef309f2a
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-27 17:53:06 +0530

    `RemoteAppointmentBooker.bookAppointment` sends appointment request to remote

--- NovaBlendSalon/NovaBlendSalonTests/Appointment API/BookAppointmentUseCaseTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/Appointment API/BookAppointmentUseCaseTests.swift
@@ -8,21 +8,63 @@
 import Foundation
 import XCTest
 
-private class RemoteAppointmentBooker {
+private protocol HTTPClient {
+    func postTo(url: URL) async throws -> (Data, HTTPURLResponse)
 }
 
-private class HTTPClientSpy {
-    var requestedURL: URL?
+private class RemoteAppointmentBooker {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func bookAppointment() async {
+        _ = try! await client.postTo(url: url)
+    }
 }
 
 final class BookAppointmentUseCaseTests: XCTestCase {
     
     func test_init_doesNotRequestAppointment() {
-        let client = HTTPClientSpy()
-        let _ = RemoteAppointmentBooker()
+        let (_,client) = makeSUT()
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_bookAppointment_sendsAppointmentRequestToURL() async {
+        let (sut, client) = makeSUT()
+        await sut.bookAppointment()
+        
+        XCTAssertFalse(client.requestedURLs.isEmpty)
+    }
+    
+    //MARK: Helper
+    private func makeSUT(url: URL = anyURL(),with result: Result<(Data, HTTPURLResponse), Error> = .success((anyData(), anyValidHTTPResponse())), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteAppointmentBooker, client: HTTPClientSpy) {
+        let client = HTTPClientSpy(result: result)
+        let sut = RemoteAppointmentBooker(url: url, client: client)
+        trackForMemoryLeak(sut)
+        trackForMemoryLeak(client)
+        return(sut, client)
     }
 }
+
+private class HTTPClientSpy: HTTPClient {
+    private(set) var requestedURLs = [URL]()
+    let result: Result<(Data, HTTPURLResponse), Error>
+    
+    init(result: Result<(Data, HTTPURLResponse), Error>) {
+        self.result = result
+    }
+    
+    func postTo(url: URL) async throws -> (Data, HTTPURLResponse) {
+        requestedURLs.append(url)
+        return try result.get()
+    }
+}
+
+
 
 
