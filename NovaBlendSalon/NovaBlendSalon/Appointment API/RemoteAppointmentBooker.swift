ab9206f019180548e7d157a7c2ba191ecde3eab9
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-08-30 19:05:43 +0530

    Move `RemoteAppointmentRequestItem` mapping logic to `RemoteAppointmentMapper`

--- NovaBlendSalon/NovaBlendSalon/Appointment API/RemoteAppointmentBooker.swift
+++ NovaBlendSalon/NovaBlendSalon/Appointment API/RemoteAppointmentBooker.swift
@@ -11,6 +11,29 @@ public class RemoteAppointmentBooker {
     private let url: URL
     private let client: HTTPClient
     
+    public enum Error: Swift.Error {
+        case connectivity
+        case AppointmentFailure
+    }
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func bookAppointment(appointment: SalonAppointment) async throws {
+        let remoteAppointmentData = try RemoteAppointmentMapper.map(appointment: appointment)
+        guard let (_, response) = try? await client.postTo(url: url, data: remoteAppointmentData) else {
+            throw Error.connectivity
+        }
+        if response.statusCode != 201 {
+            throw Error.AppointmentFailure
+        }
+    }
+}
+
+enum RemoteAppointmentMapper {
+    
     private struct RemoteAppointmentRequestItem: Encodable {
         let id: UUID
         let appointmentTime: Date
@@ -29,31 +52,14 @@ public class RemoteAppointmentBooker {
         init(appointment: SalonAppointment) {
             self.init(id: appointment.id, appointmentTime: appointment.time, phone: appointment.phone, email: appointment.email, notes: appointment.notes)
         }
-
-    }
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case AppointmentFailure
-    }
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func bookAppointment(appointment: SalonAppointment) async throws {
-        let remoteRequestItem = RemoteAppointmentRequestItem(appointment: appointment)
         
+    }
+    
+    static func map(appointment: SalonAppointment) throws -> Data {
+        let remoteRequestItem = RemoteAppointmentRequestItem.init(appointment: appointment)
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .iso8601
-        let appointmentData = try encoder.encode(remoteRequestItem)
-        
-        guard let (_, response) = try? await client.postTo(url: url, data: appointmentData) else {
-            throw Error.connectivity
-        }
-        if response.statusCode != 201 {
-            throw Error.AppointmentFailure
-        }
+        return try encoder.encode(remoteRequestItem)
     }
+    
 }
