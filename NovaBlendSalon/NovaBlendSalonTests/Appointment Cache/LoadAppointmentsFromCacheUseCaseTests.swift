fc938e85a6bed52cdb7dfceb2aa8c73aff6a10fc
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-11-12 14:00:15 +0530

    `LocalAppointmentLoader.load` throws retrieval error on retrieval failure

--- NovaBlendSalon/NovaBlendSalonTests/Appointment Cache/LoadAppointmentsFromCacheUseCaseTests.swift
+++ NovaBlendSalon/NovaBlendSalonTests/Appointment Cache/LoadAppointmentsFromCacheUseCaseTests.swift
@@ -18,7 +18,7 @@ final class LoadAppointmentsFromCacheUseCaseTests: XCTestCase {
     func test_load_requestsCacheRetrival() async throws {
         let (sut, store) = makeSUT()
         
-        sut.load()
+        try sut.load()
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
@@ -26,15 +26,31 @@ final class LoadAppointmentsFromCacheUseCaseTests: XCTestCase {
     func test_load_twice_requestsCacheRetrivalTwice() async throws {
         let (sut, store) = makeSUT()
         
-        sut.load()
-        sut.load()
+        try sut.load()
+        try sut.load()
         
         XCTAssertEqual(store.receivedMessages, [.retrieve, .retrieve])
+    }
+    
+    func test_load_failsOnRetrivalError() async {
+        let (sut, _) = makeSUT(with: retrievalError())
+        
+        do {
+            try sut.load()
+            XCTFail("Expected to throw error but got success intead")
+        } catch {
+            XCTAssertEqual(error as? LocalAppointmentLoader.Error, .retrieval)
+        }
+        
     }
     
     //MARK: Helpers
     private func makeSUT(with error: AppointmentStoreSpy.Error? = nil) -> (sut: LocalAppointmentLoader, store: AppointmentStoreSpy ) {
         let store = AppointmentStoreSpy(error: error)
         return (sut: LocalAppointmentLoader(store: store), store: store)
+    }
+    
+    private func retrievalError() -> AppointmentStoreSpy.Error {
+        return .retrievalError
     }
 }
