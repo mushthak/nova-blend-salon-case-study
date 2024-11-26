19c64f975480df77c648aaf2caf9d1f04a7a8656
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-11-26 23:03:27 +0530

    Move helper method to helper file and remove duplicates

--- NovaBlendSalon/NovaBlendSalonTests/Appointment Cache/Helpers/AppointmentCacheTestHelpers.swift
+++ NovaBlendSalon/NovaBlendSalonTests/Appointment Cache/Helpers/AppointmentCacheTestHelpers.swift
@@ -8,14 +8,6 @@
 import Foundation
 import NovaBlendSalon
 
-func makeAppointmentItem() -> Appointment {
-    return Appointment(id: UUID(),
-                            time: Date.init().roundedToSeconds(),
-                            phone: "a phone number",
-                            email: nil,
-                            notes: nil)
-}
-
 func insetionError() -> AppointmentStoreSpy.Result {
     return .failure(.insertionError)
 }
@@ -30,11 +22,4 @@ func getLocalAppointment(from model: Appointment) -> LocalAppointmentItem {
                                 phone: model.phone,
                                 email: model.email,
                                 notes: model.notes)
-}
-
-private extension Date {
-    func roundedToSeconds() -> Date {
-        let timeInterval = TimeInterval(Int(self.timeIntervalSince1970))
-        return Date(timeIntervalSince1970: timeInterval)
-    }
 }
---
+++ NovaBlendSalon/NovaBlendSalonTests/Helpers/AppointmentTestHelpers.swift
@@ -0,0 +1,54 @@
+//
+//  AppointmentTestHelpers.swift
+//  NovaBlendSalonTests
+//
+//  Created by Mushthak Ebrahim on 26/11/24.
+//
+
+import Foundation
+import NovaBlendSalon
+
+func makeAppointmentItem() -> Appointment {
+    return Appointment(id: UUID(),
+                            time: Date.init().roundedToSeconds(),
+                            phone: "a phone number",
+                            email: nil,
+                            notes: nil)
+}
+
+func makeAppointmentItem(id: UUID, time: Date, phone: String, email: String? = nil, notes: String? = nil) -> Appointment {
+    return Appointment(id: id,
+                       time: time.roundedToSeconds(),
+                       phone: phone, email: email,
+                       notes: notes)
+}
+
+func makeItem(id: UUID, time: Date, phone: String, email: String? = nil, notes: String? = nil) -> (model: Appointment, json: [String: Any]) {
+    let model = makeAppointmentItem(id: id, time: time, phone: phone, email: email, notes: notes)
+    
+    let json: [String: Any?] = [
+        "salonId" : model.id.uuidString,
+        "appointmentTime": ISO8601DateFormatter().string(from: model.time),
+        "phone": model.phone,
+        "email":  model.email,
+        "notes": model.notes
+    ]
+    
+    return (model, json.compactMapValues { $0 })
+}
+
+func makeItemsJSON(items: [[String : Any]]) -> Data {
+    let json = ["appointments": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+func makeItemJSON(item: [String : Any]) -> Data {
+    return try! JSONSerialization.data(withJSONObject: item)
+}
+
+private extension Date {
+    func roundedToSeconds() -> Date {
+        let timeInterval = TimeInterval(Int(self.timeIntervalSince1970))
+        return Date(timeIntervalSince1970: timeInterval)
+    }
+}
